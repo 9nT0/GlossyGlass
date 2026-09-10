5c2e8a96fde951a201302d811f40ff14085919fd
killswitch <ajjornyreeves2@gmail.com>
2026-09-10 16:08:37 -0500

    ,

--- GlassLiquidTabBar.swift
+++
@@ -1,7 +0,0 @@
-import UIKit
-@objc public final class GlassLiquidTabBar: NSObject {
-    @objc public static let shared = GlassLiquidTabBar()
-    private override init() { super.init() }
-    @objc public func start() { GlassChromeCoordinator.shared.start() }
-    @objc public func refresh() { GlassChromeCoordinator.shared.apply(reason: "liquidTab") }
-}
