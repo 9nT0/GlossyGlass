e6ebf39cb94e74963c0e38df4c3d3d97161bd10e
killswitch <ajjornyreeves2@gmail.com>
2026-09-13 14:21:12 -0500

    .

--- GlassChromeCoordinator.swift
+++
@@ -1,4 +0,0 @@
-import UIKit
-
-// Compatibility: full type defined in GlassUICoordinator.swift
-// Kept as a compile unit so older project refs stay valid.
--- GlassFloatingTabGlass.swift
+++
@@ -1,5 +0,0 @@
-import UIKit
-@objc public final class GlassFloatingTabGlass: NSObject {
-    @objc public static let shared = GlassFloatingTabGlass()
-    @objc public func attachIfNeeded() { GlassDock.shared.attachIfNeeded() }
-}
